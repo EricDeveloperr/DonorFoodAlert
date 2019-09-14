90ed2ee6e7e94f222391a1a33b7715074a59daa2
Jacky He <heja@utschools.ca>
2019-09-14 16:27:06 -0400

    This is a working Log in

--- app/src/main/java/com/example/donoralert/MainActivity.java
+++ app/src/main/java/com/example/donoralert/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.View;
 import android.widget.*;
 import android.content.*;
 
+import com.google.firebase.auth.*;
+
 //main activity is now the welcome page
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         receiver = findViewById(R.id.receiver);
         donor = findViewById(R.id.donor);
+        mAuth = FirebaseAuth.getInstance();
         //create an intent
         toLogIn = new Intent(this, RoleSelection.class);
         preferences = this.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
@@ -57,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 editor.putBoolean("role", true);
                 editor.commit();
-                startActivity (toLogIn);
+                toNextView();
             }
         });
 
@@ -81,5 +86,20 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("2", "Error adding document", e);
             }
         });*/
+    }
+
+    @Override
+    public void onStart () {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null)
+        {
+            toNextView();
+            //go to the next page
+        }
+    }
+
+    public void toNextView () {
+        startActivity(toLogIn);
     }
 }
