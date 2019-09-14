a51553cf3dff5bb818415b2ecb3eed4afc9d522b
EricProgrammer <ESprogrammer77@gmail.com>
2019-09-14 17:55:53 -0400

    - Implemented and Created new class and screen for final overview of users account and data.  - Implemented intent

--- app/src/main/java/com/example/donoralert/FoodConfirmation.java
+++ app/src/main/java/com/example/donoralert/FoodConfirmation.java
@@ -2,13 +2,31 @@ package com.example.donoralert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FoodConfirmation extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_confirmation);
+
+        button = findViewById(R.id.confirmButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openFoodWaitList();
+            }
+        });
+    }
+
+    public void openFoodWaitList(){
+        Intent intent = new Intent(this, FoodWaitList.class);
+        startActivity(intent);
     }
 }
