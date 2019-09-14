c9f1276b8e5d4ce736b3ea75f1fb330a57ed5993
Jacky He <heja@utschools.ca>
2019-09-14 11:39:30 -0400

    firebasesetup

--- app/src/main/java/com/example/donoralert/MainActivity.java
+++ app/src/main/java/com/example/donoralert/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.donoralert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import java.util.*;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.*;
+import com.google.firebase.*;
+import android.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this is just for testing
+        FirebaseApp.initializeApp(this);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map <String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put ("last", "Lovelace");
+        user.put("born", 1815);
+
+        db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d("1", "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("2", "Error adding document", e);
+            }
+        });
     }
 
 }
