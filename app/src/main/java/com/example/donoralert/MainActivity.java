31ee4c4caf8c87ee8c16a271945fb00d0f8b6233
Jacky He <heja@utschools.ca>
2019-09-14 10:48:58 -0400

    test

--- app/src/main/java/com/example/donoralert/MainActivity.java
+++ app/src/main/java/com/example/donoralert/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.*;
+
+import com.google.firebase.firestore.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map <String, Object> user = new HashMap<>();
     }
 
     protected void testMethod (){}
